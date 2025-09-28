f3faa490db0b6d4ea1176c1527a9f8f695b9961d
ruslana228 <lanka.banka4@gmail.com>
2025-09-28 17:57:30 +0300

    просто проект создала

---
+++ screenSaver/MainForm.cs
@@ -0,0 +1,15 @@
+namespace screenSaver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
